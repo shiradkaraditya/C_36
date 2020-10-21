91e8d7a40a67abedd763314c220375a3dc89cce3
shiradkaraditya <shiradkaraditya04@gmail.com>
2020-10-21 14:41:57 +0530

    Car game

--- sketch.js
+++ sketch.js
@@ -1,50 +1,30 @@
-var ball;
-var database,position;
+var canvas,backgroundImage;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance =0;
+var database;
+var form,player,game;
 
 function setup(){
-    database=firebase.database();
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    var ballPosition = database.ref('ball/position');
-    ballPosition.on("value",readPosition,showError);
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game =new Game();
+    game.getState();
+    game.start();
 
 }
 
+function draw() {
+if(playerCount === 4){
+    game.update(1);
 
-function draw(){
-    background("white");
-    if(position !== undefined){
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
 }
+if(gameState === 1){
+    clear ();
+    game.play();
 }
 
-function writePosition(x,y){
-   database.ref('ball/position').set({
-       'x': position.x+x,
-       'y': position.y+y
-   })
-}
 
-function readPosition(data){
-    position = data.val();
 
-    ball.x = position.x;
-    ball.y = position.y;
-}
-
-function showError(){
-    console.log("error in writing to the database");
 }
